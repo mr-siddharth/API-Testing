ec6990bc634a57ff2411b7722b445f54dc9466b9
mr-siddharth <siddharth2k007@gmail.com>
2021-04-19 11:25:02 +0530

    added new files and tests

--- testcases/test_create.py
+++ testcases/test_create.py
@@ -1,13 +1,12 @@
 import testlib.helpers.customers as customers
 from testlib import lib
 
-
 class TestCreateCustomer:
 
-    def test_create_customer(self):
+    def test_smoke_create_customer(self):
         response = customers.create_customer(email=lib.get_random_email(), password=lib.get_random_string())
         print(response)
         print('-' * 30)
         print(response.request.body)
         print('-' * 30)
-        print(response.text)
+        print(response.json())
